ff712618ecd48c1ec061c5609c898f1573585765
Alexey Shtomov <alexeyshtomov2001@gmail.com>
2023-07-18 17:52:31 +1000

    фиксация

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,20 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
 
-  return { min: min, max: max, avg: avg };
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+    if (num < min) {
+      min = num;
+    }
+    if (num > max) {
+      max = num;
+    }
+    sum += num;
+    
+    return { min, max, sum };
+}
 }
 
 function summElementsWorker(...arr) {
