21ca67213979f672478d50e0832e97737dbc3bc7
Alexey Shtomov <alexeyshtomov2001@gmail.com>
2023-08-06 15:07:12 +0300

    56

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -21,3 +21,19 @@
 
   console.log(getArrayParams(1, 2, 3, 4, 5)); 
   console.log(getArrayParams(10, -5, 7, 12));
+
+  
+  function summElementsWorker(arr) {
+    if (!arr || arr.length === 0) return 0;
+  
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+  
+    return sum;
+  }
+    const arr = [1, 2, 3, 4, 5];
+    const sum = summElementsWorker(arr);
+
+    console.log(sum);  
