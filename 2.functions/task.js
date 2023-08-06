bedde637c9fd7f2c9dd0446505f4a87bf9ebfb4d
Alexey Shtomov <alexeyshtomov2001@gmail.com>
2023-08-06 16:09:21 +0300

    56464

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -104,5 +104,26 @@
   
   console.log(averageEvenElementsWorker([1, 2, 3, 4, 5, 6, 7, 8, 9])); 
   console.log(averageEvenElementsWorker([15, 97, 85, 64, 67, 10, 69, 40, 15, 35]));
+
+  function makeWork(arrOfArr, func) {
+    let maxWorkerResult = -Infinity;
+  
+    for (let i = 0; i < arrOfArr.length; i++) {
+      const result = func(...arrOfArr[i]);
+  
+      if (result > maxWorkerResult) {
+        maxWorkerResult = result;
+      }
+    }
+  
+    return maxWorkerResult;
+  }
+  const arr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]];
+  console.log(makeWork(arr, summElementsWorker));
+  console.log(makeWork(arr, differenceMaxMinWorker));
+  console.log(makeWork(arr, differenceEvenOddWorker)); 
+  console.log(makeWork(arr, averageEvenElementsWorker));  
+
+
     
  
