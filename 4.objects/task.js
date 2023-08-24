2d9b8e18f4df55a6e76ea61022ec5183d0d08649
Alexey Shtomov <alexeyshtomov2001@gmail.com>
2023-08-24 22:51:10 +0300

    4545

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -3,55 +3,49 @@ function Student(name, gender, age) {
     this.gender = gender;
     this.age = age;
     this.marks = [];
-  
 }
-Student.prototype.setSubject = function Student(subjectName){
 
+Student.prototype.setSubject = function(subjectName) {
     this.subject = subjectName;
 }
-Student.prototype.addMarks = function Student (...marks){
 
-    if(this.marks == undefined) {
-        console.log("Студент отчислен и не имеет каких-либо оценок");
+Student.prototype.addMarks = function(...marksToAdd) {
+    if (this.marks === undefined) {
+        console.log("Студент отчислен и не имеет каких-либо оценок!");
         return;
     }
-    this.marks.push(...marks)
-
-
+    this.marks.push(...marksToAdd);
 }
-Student.prototype.getAverage = function Student() {
 
-    
-    if(this.average == undefined || this.marks.length === 0) {
+Student.prototype.getAverage = function() {
+    if (this.marks === undefined || this.marks.length === 0) {
         return 0;
     }
     const sum = this.marks.reduce((total, mark) => total + mark, 0);
     return sum / this.marks.length;
-    
-  
 }
 
-Student.prototype.exclude = function Student (reason) {
-
+Student.prototype.exclude = function(reason) {
     this.subject = undefined;
     this.marks = undefined;
-    this.exclude = reason;
-  
+    this.excluded = reason;
 }
 
-const student1 = new Student("Витя", "мужской", 20);
-const student2 = new Student("Паша", "мужской", 23);
-const marks_student = new Student(...marks);
-student1.addMarks(4,5,4,5);
-student2.addMarks(4,5,4,5);
-student1.setSubject("Algebra");
-student1.setSubject("Geometry");
+// Создаем экземпляр объекта студента
+const student1 = new Student("Ivan", "Male", 32);
 
-console.log(student1.getAverage());
-console.log(student2.getAverage());
-console.log("Средний балл:", student1.getAverage());
-student1.exclude("Плохая учеба");
+// Устанавливаем предмет
+student1.setSubject("Math");
+
+// Добавляем оценки
+student1.addMarks(85, 90, 78);
+
+// Получаем средний балл
+console.log("Средний балл студента:", student1.getAverage());
+
+// Исключаем студента
+
+student1.exclude("Плохая успеваемость");
 console.log("Студент был исключен:", student1);
-
 
 
