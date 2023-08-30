24a715555987a03bed3c011b40d379efceed7c94
Alexey Shtomov <alexeyshtomov2001@gmail.com>
2023-08-30 07:38:19 +0300

    7865

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -62,4 +62,65 @@ class PrintEditionItem {
     }
   }
    
+   class Library {
+  constructor(name) {
+    this.name = name;
+    this.books = [];
+  }
+}
+
+  addBook(book) {
+    if (book.state > 30) {
+      this.books.push(book);
+    }
+}
+
+findBookBy(type, value) {
+    const book = this.books.find((book) => book[type] === value);
+    return book || null;
+}
+
+giveBookByName(bookName) {
+    const index = this.books.findIndex((book) => book.title === bookName);
+    if (index === -1) {
+      return null;
+    } else {
+      return this.books.splice(index, 1)[0];
+    }
+}
+
+
+const library = new Library("Главная библиотека");
+
+
+const book1 = new Book("Война и мир", "Лев Толстой", 1869);
+const book2 = new Book("Преступление и наказание", "Федор Достоевский", 1866);
+const textbook = new Textbook("Высшая математика", "Иван Иванов", 1980, "математика");
+const magazine = new Magazine("Вокруг света", 1925, 5);
+
+library.addPrintEdition(book1);
+library.addPrintEdition(book2);
+library.addPrintEdition(textbook);
+library.addPrintEdition(magazine);
+
+
+const book3 = library.findBookByYear(1919);
+if (!book3) {
+  const newBook = new Book("Новая книга", "Неизвестный автор", 1919);
+  library.addPrintEdition(newBook);
+}
+
+
+const borrowedBook = library.givePrintEdition();
+
+
+borrowedBook.isDamaged = true;
+
+
+library.restorePrintEdition(borrowedBook);
+
+
+library.addPrintEdition(borrowedBook);
+
+
    
