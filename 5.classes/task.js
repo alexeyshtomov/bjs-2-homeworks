d88c3be7fe04d1a8841d4438c312d0f7f0cf4466
Alexey Shtomov <alexeyshtomov2001@gmail.com>
2023-08-30 07:42:22 +0300

    2313

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,126 +1,108 @@
 class PrintEditionItem {
-    constructor(name, releaseDate, pagesCount) {
-      this.name = name;
-      this.releaseDate = releaseDate;
-      this.pagesCount = pagesCount;
-      this.type = null;
-      this.state = 100;
-    }
-  
-    fix() {
-      this.state = this.state * 1.5;
-    }
-  
-    set state(state) {
-      if (state < 0) {
-        this._state = 0;
-      } else if (state > 100) {
-        this._state = 100;
-      } else {
-        this._state = state;
-      }
-    }
-  
-    get state() {
-      return this._state;
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.type = null;
+    this.state = 100;
+  }
+
+  fix() {
+    this.state *= 1.5;
+  }
+
+  set state(state) {
+    if (state < 0) {
+      this._state = 0;
+    } else if (state > 100) {
+      this._state = 100;
+    } else {
+      this._state = state;
     }
   }
-  
-  class Magazine extends PrintEditionItem {
-    constructor(name, releaseDate, pagesCount) {
-      super(name, releaseDate, pagesCount);
-      this.type = "magazine";
-    }
+
+  get state() {
+    return this._state;
   }
-  
-  class Book extends PrintEditionItem {
-    constructor(author, name, releaseDate, pagesCount) {
-      super(name, releaseDate, pagesCount);
-      this.type = "book";
-      this.author = author;
-    }
+}
+
+class Magazine extends PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.type = "magazine";
   }
-  
-  class NovelBook extends Book {
-    constructor(author, name, releaseDate, pagesCount) {
-      super(author, name, releaseDate, pagesCount);
-      this.type = "novel";
-    }
+}
+
+class Book extends PrintEditionItem {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.type = "book";
+    this.author = author;
   }
-  
-  class FantasticBook extends Book {
-    constructor(author, name, releaseDate, pagesCount) {
-      super(author, name, releaseDate, pagesCount);
-      this.type = "fantastic";
-    }
+}
+
+class Textbook extends Book {
+  constructor(author, name, releaseDate, pagesCount, subject) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "textbook";
+    this.subject = subject;
   }
-  
-  class DetectiveBook extends Book {
-    constructor(author, name, releaseDate, pagesCount) {
-      super(author, name, releaseDate, pagesCount);
-      this.type = "detective";
-    }
-  }
-   
-   class Library {
+}
+
+class Library {
   constructor(name) {
     this.name = name;
     this.books = [];
   }
-}
 
-  addBook(book) {
-    if (book.state > 30) {
-      this.books.push(book);
+  addPrintEdition(printEdition) {
+    if (printEdition.state > 30) {
+      this.books.push(printEdition);
     }
-}
+  }
 
-findBookBy(type, value) {
-    const book = this.books.find((book) => book[type] === value);
+  findBookByYear(releaseDate) {
+    const book = this.books.find((book) => book.releaseDate === releaseDate);
     return book || null;
-}
+  }
 
-giveBookByName(bookName) {
-    const index = this.books.findIndex((book) => book.title === bookName);
-    if (index === -1) {
+  givePrintEdition() {
+    if (this.books.length === 0) {
       return null;
     } else {
-      return this.books.splice(index, 1)[0];
+      return this.books.pop();
     }
-}
+  }
 
+  restorePrintEdition(printEdition) {
+    printEdition.fix();
+  }
+}
 
 const library = new Library("Главная библиотека");
 
-
-const book1 = new Book("Война и мир", "Лев Толстой", 1869);
-const book2 = new Book("Преступление и наказание", "Федор Достоевский", 1866);
-const textbook = new Textbook("Высшая математика", "Иван Иванов", 1980, "математика");
-const magazine = new Magazine("Вокруг света", 1925, 5);
+const book1 = new Book("Лев Толстой", "Война и мир", 1869, 1225);
+const book2 = new Book("Федор Достоевский", "Преступление и наказание", 1866, 672);
+const textbook = new Textbook("Иван Иванов", "Высшая математика", 1980, 256, "математика");
+const magazine = new Magazine("Вокруг света", 1925, 72);
 
 library.addPrintEdition(book1);
 library.addPrintEdition(book2);
 library.addPrintEdition(textbook);
 library.addPrintEdition(magazine);
 
-
 const book3 = library.findBookByYear(1919);
 if (!book3) {
-  const newBook = new Book("Новая книга", "Неизвестный автор", 1919);
+  const newBook = new Book("Неизвестный автор", "Новая книга", 1919, 150);
   library.addPrintEdition(newBook);
 }
 
-
 const borrowedBook = library.givePrintEdition();
 
-
-borrowedBook.isDamaged = true;
-
+borrowedBook.state = 30;
 
 library.restorePrintEdition(borrowedBook);
 
-
 library.addPrintEdition(borrowedBook);
-
 
    
