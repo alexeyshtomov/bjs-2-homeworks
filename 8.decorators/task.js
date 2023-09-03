55f102e327a639245bde6bffbca938444eec27ed
Alexey Shtomov <alexeyshtomov2001@gmail.com>
2023-09-03 21:33:05 +0300

    435

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -45,50 +45,44 @@ console.log(upgraded(1, 2, 3)); // Вычисляем: 9 (снова вычис�
 
 
 //Задача № 2
-function debounceDecoratorNew(func, interval) {
+function debounceDecoratorNew(func, delay) {
   let timeoutId;
   let count = 0;
   let allCount = 0;
 
   function wrapper(...args) {
     allCount++;
-
     if (!timeoutId) {
-      count++;
       func(...args);
+      count++;
+    } else {
+      clearTimeout(timeoutId);
     }
-
-    clearTimeout(timeoutId);
 
     timeoutId = setTimeout(() => {
       timeoutId = null;
-    }, interval);
+    }, delay);
   }
 
-  Object.defineProperty(wrapper, 'count', {
-    get: () => count,
-  });
-
-  Object.defineProperty(wrapper, 'allCount', {
-    get: () => allCount,
-  });
+  wrapper.count = () => count;
+  wrapper.allCount = () => allCount;
 
   return wrapper;
 }
 
-// Пример использования
+// Пример использования и тестов
 const sendSignal = (signalOrder, delay) => console.log("Сигнал отправлен", signalOrder, delay);
 const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
 
-setTimeout(() => upgradedSendSignal(1, 0), 0);
-setTimeout(() => upgradedSendSignal(2, 300), 300);
-setTimeout(() => upgradedSendSignal(3, 900), 900);
-setTimeout(() => upgradedSendSignal(4, 1200), 1200);
-setTimeout(() => upgradedSendSignal(5, 2300), 2300);
-setTimeout(() => upgradedSendSignal(6, 4400), 4400);
-setTimeout(() => upgradedSendSignal(7, 4500), 4500);
+setTimeout(() => upgradedSendSignal(1, 0)); // Сигнал отправлен + будет запланирован асинхронный запуск, который будет проигнорирован, так как следующий сигнал отменит предыдущий (300 - 0 < 2000)
+setTimeout(() => upgradedSendSignal(2, 300), 300); // проигнорировано, так как следующий сигнал отменит предыдущий (900 - 300 < 2000)
+setTimeout(() => upgradedSendSignal(3, 900), 900); // проигнорировано, так как следующий сигнал отменит предыдущий (1200 - 900 < 2000)
+setTimeout(() => upgradedSendSignal(4, 1200), 1200); // проигнорировано, так как следующий сигнал отменит предыдущий (2300 - 1200 < 2000)
+setTimeout(() => upgradedSendSignal(5, 2300), 2300); // Сигнал отправлен, так как следующий вызов не успеет отменить текущий: 4400-2300=2100 (2100 > 2000)
+setTimeout(() => upgradedSendSignal(6, 4400), 4400); // проигнорировано, так как следующий сигнал отменит предыдущий (4500 - 4400 < 2000)
+setTimeout(() => upgradedSendSignal(7, 4500), 4500); // Сигнал будет отправлен, так как последний вызов debounce декоратора (спустя 4500 + 2000 = 6500) 6,5с
 
 setTimeout(() => {
-  console.log(upgradedSendSignal.count); // Выведет: 3
-  console.log(upgradedSendSignal.allCount); // Выведет: 7
+  console.log(upgradedSendSignal.count()); // было выполнено 3 отправки сигнала
+  console.log(upgradedSendSignal.allCount()); // было выполнено 7 вызовов декорированной функции
 }, 7000);
