483825e11d22ca661e7a52d023b3a4fd2c7214f2
Alexey Shtomov <alexeyshtomov2001@gmail.com>
2023-09-03 18:50:09 +0300

    888

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,5 +1,48 @@
 //Задача № 1
+
+const md5 = require('js-md5'); 
+
 function cachingDecoratorNew(func) {
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = md5(JSON.stringify(args)); 
+
+    
+    const objectInCache = cache.find((item) => item.hash === hash);
+
+    if (objectInCache) { // Если элемент найден в кеше
+      console.log("Из кеша: " + objectInCache.value);
+      return "Из кеша: " + objectInCache.value;
+    }
+
+    const result = func(...args); // Вычисляем результат
+    cache.push({ hash, value: result }); // Добавляем результат в кеш
+
+    if (cache.length > 5) {
+      cache.shift(); 
+    }
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+
+  return wrapper;
+}
+
+
+const addAndMultiply = (a, b, c) => (a + b) * c;
+const upgraded = cachingDecoratorNew(addAndMultiply);
+
+console.log(upgraded(1, 2, 3)); // Вычисляем: 9
+console.log(upgraded(1, 2, 3)); // Из кеша: 9
+console.log(upgraded(2, 2, 3)); // Вычисляем: 12
+console.log(upgraded(3, 2, 3)); // Вычисляем: 15
+console.log(upgraded(4, 2, 3)); // Вычисляем: 18
+console.log(upgraded(5, 2, 3)); // Вычисляем: 21
+console.log(upgraded(6, 2, 3)); // Вычисляем: 24 (при этом кеш для 1, 2, 3 уничтожается)
+console.log(upgraded(1, 2, 3)); // Вычисляем: 9 (снова вычисляем, кеша нет)
+
   
 }
 
