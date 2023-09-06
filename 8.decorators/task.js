2e11bb578374e62b3aec612454d293a9020afd3b
Alexey Shtomov <alexeyshtomov2001@gmail.com>
2023-09-06 22:38:35 +0300

    777

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -23,3 +23,35 @@ function cachingDecoratorNew(func) {
 
   return wrapper;
 }
+
+function debounceDecoratorNew(func, delay) {
+  let timeoutId;
+  let count = 0;
+  let allCount = 0;
+
+  function wrapper(...args) {
+    clearTimeout(timeoutId);
+
+    if (!timeoutId) {
+      
+      const result = func(...args);
+      count++;
+      allCount++;
+      wrapper.count = count;
+      wrapper.allCount = allCount; 
+      return result;
+    }
+
+    timeoutId = setTimeout(() => {
+      const result = func(...args);
+      count++;
+      allCount++;
+      timeoutId = undefined;
+      wrapper.count = count; 
+      wrapper.allCount = allCount; 
+      console.log("Сигнал отправлен", args[0], args[1]);
+    }, delay);
+  }
+
+  return wrapper;
+}
